152583c3d377f9c0d91e31d5cc9f17c53aab2d1b
ShafkatSharif <shafkatshari12@gmail.com>
2025-09-02 19:39:37 +0600

    Update .gitignore and initial project setup

---
+++ chatbot.py
@@ -0,0 +1,36 @@
+from langchain_community.llms import ollama
+from pdf_processing import vector
+from langchain.chains import retrieval_qa
+from langchain.prompts import PromptTemplate
+
+MODEL = "mistral:7b"
+llm= ollama(model=MODEL)
+
+template = """
+Answer the question based on only physics content which are given. If you can't 
+answer the question, reply "I don't know".
+
+Context: {context}
+
+Question: {question}
+"""
+
+prompt = PromptTemplate.from_template(template)
+prompt.format(context="Here is some context", question="Here is a question")
+
+
+
+
+def get_response(question):
+    response = llm.invoke(question)
+    return response
+
+print("Welcome to Physics Chatbot!")
+
+while True:
+    text = input("Ki jante chas? : ")
+    if text in ["exit", "quit"]:
+         print("Hedar pola tumi!")
+         break
+    answer = get_response(text)
+    print(answer)
---
+++ test.py
@@ -0,0 +1,40 @@
+import os
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain.schema import Document
+from langchain.text_splitter import CharacterTextSplitter
+import PyPDF2
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+current_directory = os.path.dirname(os.path.abspath(__file__))
+vector_directory = os.path.join(current_directory, "db", "chroma_db_physics")
+
+def pdf_text_loader(file_path):
+    pdf_text = ""
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            pdf_text += page.extract_text()
+    return pdf_text
+
+def store_in_vector_store(docs):
+    # Initialize Chroma and store the documents
+    vector_store = Chroma.from_documents(
+        collection_name="physics", 
+        documents=docs, 
+        embedding=embeddings, 
+        persist_directory=vector_directory
+    )
+    vector_store.persist()  # Save the vectors to disk
+    print("Documents have been stored in the vector store.")
+
+def create_chunks(texts):
+    splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    return splitter.create_documents(texts)
+
+
+if __name__ == "__main__":
+    pdf_text = pdf_text_loader("physics_notes.pdf")
+    docs = create_chunks(pdf_text)
+    store_in_vector_store(docs)
